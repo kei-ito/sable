beaa1d5c78701828048926a8a8defae7ba3e5cc5
Kei Ito <kei.itof@gmail.com>
2018-07-07 17:46:17 +0900

    remove asyncForEach

--- src/async-for-each/index.js
+++
@@ -1,5 +0,0 @@
-exports.asyncForEach = function asyncForEach(list, asyncFn, index = 0) {
-	return index < list.length
-	? asyncFn(list[index], index, list, () => asyncForEach(list, asyncFn, index + 1))
-	: Promise.resolve();
-};
--- src/middleware-static-file/index.js
+++ src/middleware-static-file/index.js
@@ -1,24 +1,22 @@
 const path = require('path');
-const {asyncForEach} = require('../async-for-each');
 const {indexPage} = require('./index-page');
 const {serveFile} = require('./serve-file');
 
-exports.staticFile = function staticFile(req, res, next, server) {
+exports.staticFile = async function staticFile(req, res, next, server) {
 	const {pathname} = req.parsedURL;
-	return asyncForEach(server.documentRoot, (directory, index, directories, next) => {
+	for (const directory of server.documentRoot) {
 		const filePath = path.join(directory, ...pathname.split('/'));
-		return (pathname.endsWith('/') ? indexPage : serveFile)(filePath, req, res, server)
-		.catch((error) => {
-			if (error && error.code === 'ENOENT') {
-				return next();
+		try {
+			await (pathname.endsWith('/') ? indexPage : serveFile)(filePath, req, res, server);
+			break;
+		} catch (error) {
+			if (!(error && error.code === 'ENOENT')) {
+				throw error;
 			}
-			throw error;
-		});
-	})
-	.then(() => {
-		if (!res.finished) {
-			res.statusCode = 404;
-			res.end();
 		}
-	});
+	}
+	if (!res.finished) {
+		res.statusCode = 404;
+		res.end();
+	}
 };

4d19bc5efa6235bd1390d28ac7d455db435f7157
Kei Ito <kei.itof@gmail.com>
2018-08-07 18:43:59 +0900

    remove old-test

--- old-test/lib/directories/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const test = path.join(__dirname, '..', '..');
-const src = path.join(test, 'src');
-const temp = path.join(test, 'temp', `${Date.now()}`);
-module.exports = {
-	test,
-	src,
-	temp,
-};

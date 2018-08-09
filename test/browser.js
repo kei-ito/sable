e6487d56b680bd373601fb0ee62244841cdd3449
Kei Ito <kei.itof@gmail.com>
2018-08-09 11:09:18 +0900

    print port numbers

--- test/browser.js
+++ test/browser.js
@@ -52,6 +52,8 @@ t.test('Sync', {timeout: timeout * capabilities.length}, (t) => {
 
 	capabilities.forEach((capability, index) => {
 		t.test(`Capability#${index}`, {timeout, bail: true}, async (t) => {
+			t.ok(1, `port: ${sableServer.server.address().port}`);
+			t.ok(1, `wsport: ${sableServer.wss.address().port}`);
 			for (const key of Object.keys(capability)) {
 				t.ok(1, `${key}: ${capability[key]}`);
 			}

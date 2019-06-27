8a6abf7c7ea7dc6f1be458a0914ebe600a83c5b1
Kei Ito <kei.itof@gmail.com>
2019-06-27 17:42:49 +0900

    feat: support all options

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,31 @@ import * as http from 'http';
 import * as connect from 'connect';
 import * as staticLivereload from 'middleware-static-livereload';
 
-export interface ISableOptions {
-    documentRoot?: string | Array<string>,
+export interface ISableOptions extends staticLivereload.IOptions {
+    /**
+     * The first argument of server.listen()
+     * https://nodejs.org/api/net.html#net_server_listen_port_host_backlog_callback
+     * @default 4000
+     */
     port?: number,
+    /**
+     * The second argument of server.listen()
+     * https://nodejs.org/api/net.html#net_server_listen_port_host_backlog_callback
+     * @default undefined
+     */
     host?: string,
-    index?: string,
-    noWatch?: boolean,
 }
 
-export const startServer = ({
-    documentRoot = process.cwd(),
-    port = 4000,
-    host,
-    index = 'index.html',
-    noWatch = false,
-}: ISableOptions = {}): Promise<http.Server> => new Promise((resolve, reject) => {
+export const startServer = (options: ISableOptions = {}): Promise<http.Server> => new Promise((resolve, reject) => {
     const app = connect();
-    app.use(staticLivereload.middleware({
-        documentRoot,
-        index,
-        watch: !noWatch,
-    }));
+    app.use(staticLivereload.middleware(options));
     const server = http.createServer(app);
     server.once('listening', () => {
         const addressInfo = server.address();
         if (addressInfo && typeof addressInfo === 'object') {
             const {address, family, port} = addressInfo;
             const portSuffix = port === 80 ? '' : `:${port}`;
-            const hostname = host || (portSuffix && family === 'IPv6' ? `[${address}]` : address);
+            const hostname = options.host || (portSuffix && family === 'IPv6' ? `[${address}]` : address);
             process.stdout.write(`http://${hostname}${portSuffix}\n`);
         }
         resolve(server);
@@ -45,5 +42,5 @@ export const startServer = ({
         })
         .listen(port, host);
     };
-    listen(port, host);
+    listen(options.port || 4000, options.host);
 });
